b1f49de54c9d946a4ce7d629b3de7e9ce163d346
Dave Boyd <daveboyd777@gmail.com>
2024-03-06 18:08:55 -0500

    Formatted and linted (Black)

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,8 +1,35 @@
 #!/usr/bin/env python3
-"""Say Hello""" 
+"""
+Say Hello
+
+Usage: hello.py [-h] [-n name]
+
+Dave Boyd
+2024-03-06
+
+"""
 import argparse
 
-parser = argparse.ArgumentParser(description='Say Hello')
-parser.add_argument('-n', '--name', metavar='name', default='World', help='usage: hello.py [-h] [-n name]')  
-args = parser.parse_args()
-print(f'Hello, {args.name}!')   # This is the main function that prints the output
+
+def get_args():
+    """Get the command-line arguments"""
+    parser = argparse.ArgumentParser(description="Say Hello")
+    parser.add_argument(
+        "-n",
+        "--name",
+        metavar="name",
+        default="World",
+        help="usage: hello.py [-h] [-n name]",
+    )
+    return parser.parse_args()
+
+
+def main():
+    """Execute the main function."""
+    args = get_args()
+    name = args.name
+    print("Hello, " + name + "!")
+
+
+if __name__ == "__main__":
+    main()
